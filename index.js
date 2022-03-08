cf3e99ae510e5b22dc4f6eb827cd9bd93eec99ee
Manh Ngo <manhnq097@gmail.com>
2022-03-08 22:21:07 +0700

    first comnmit

---
+++ index.js
@@ -0,0 +1,5 @@
+// Define variabe
+var myName = 'Manh Ngo';
+// Function
+var sum = function (a, b, c) { return a + b + c; };
+console.log(sum(1, 2, 3));
---
+++ index.ts
@@ -0,0 +1,7 @@
+// Define variabe
+var myName: string = 'Manh Ngo';
+
+// Function
+const sum = (a: number, b: number, c: number): number => a + b + c;
+
+console.log(sum(1, 2, 3));
